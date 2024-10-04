598c988a2a4ecbf92891f3262bfcd2b91cc86948
Stefan Bergstein <stefan.bergstein@gmail.com>
2024-10-04 14:38:52 +0200

    bug

--- helloworld.py
+++ helloworld.py
@@ -9,13 +9,13 @@ def hello_world():
     app.logger.warning(request.data)
     # Respond with another event (optional)
 
-
-#    response = make_response({"msg": "Hi from helloworld-python app!"})
-#    response.headers["Ce-Id"] = str(uuid.uuid4())
-#    response.headers["Ce-specversion"] = "0.3"
-#    response.headers["Ce-Source"] = "knative/eventing/samples/hello-world"
-#    response.headers["Ce-Type"] = "dev.knative.samples.hifromknative"
-#    return response
+    #    response = make_response({"msg": "Hi from helloworld-python app!"})
+    #    response.headers["Ce-Id"] = str(uuid.uuid4())
+    #    response.headers["Ce-specversion"] = "0.3"
+    #    response.headers["Ce-Source"] = "knative/eventing/samples/hello-world"
+    #    response.headers["Ce-Type"] = "dev.knative.samples.hifromknative"
+    #    return response
+    return
 
 
 if __name__ == "__main__":
